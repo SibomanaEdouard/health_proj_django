5bed4ccbae42e8c1764411de98c61b4756f3ff7c
SibomanaEdouard <sibomanaedouard974@gmail.com>
2024-10-19 23:08:22 +0200

    finish user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# This is user models
+class Users(models.Model):
+    uuid = models.UUIDField()
+    firstname = models.CharField(max_length=255, null=False)
+    lastname = models.TextField(max_length=255, null=False)
+    email = models.EmailField(max_length=255,null=False,unique=True)
+    phone = models.TextField(max_length=10,null=False,unique=True)
+    password = models.CharField(null=False)
+    createdAt = models.DateTimeField(default=timezone.now, editable=False)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class Meta:
+    db_table='users'
+
